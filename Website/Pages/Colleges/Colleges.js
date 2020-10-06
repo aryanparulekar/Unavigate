ae2511f8812132bbdf404e8e316637fcbdcbcfc7
aryanparulekar <72474097+aryanparulekar@users.noreply.github.com>
2020-10-06 18:37:44 -0400

    Add files via upload

--- Website/Pages/Colleges/Colleges.js
+++ Website/Pages/Colleges/Colleges.js
@@ -1,55 +1,53 @@
 const collegeData = [
-  {
-    name: "UNC"[
       {
+        name: "UNC",
         student: "Bob Herrald",
         year: "Sophomore",
-      }
-    ]
-  },
-  {
-    name: "UCLA"[
+      },
       {
+        name: "UCLA",
         student: "John Herrald",
         year: "Junior",
-      }
-    ]
-
-  }
+      },
 ];
 
 
 
 
 
-  document.getElementById("ma");
 
-window.onload = function(){
 
+
+function college(collegename){
   document.getElementById("ma").innerHTML=`
   ${collegeData.map(function(college){
-      return `
-
-      
-
-
-      
-      <link rel="stylesheet" href="/css/bootstrap.min.css">
-
-        <div class = "col-sm-6">
-          <h2> ${college.name} </h2>
+    document.getElementById('UNC').style.opacity = 0;  
+    document.getElementById('UCLA').style.opacity = 0;  
+    if (collegename == college.name) {
+        return `
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+  
+        <div class = "col-sm-12">
+        <h2> <br> </br> </h2>
         </div>
 
-        <div class = "col-sm-6">
-          <h2> [domdlfm] </h2>
-        </div>
+          <div class = "col-sm-12">
+            <h2> ${college.name} ${college.student} </h2>
+          </div>
+  
 
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-      <link rel="stylesheet" href="/Pages/Colleges/Colleges.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-      `
+  
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/Pages/Colleges/Colleges.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        `
+      }
+
       }
     )}
-  `;
+    `;
+    
+    // var  div = document.getElementById('button');
+    // div.parentNode.removeChild(div);
 }
